5984c9663e7ed01205932d115424d0368b32bf7b
BTechWithLaw <BTechWithLaw@gmail.com>
2023-01-02 12:31:22 +0530

    Model with List and Details demo

---
+++ MVCfullDemo2023/MVCfullDemo2023/Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using MVCfullDemo2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCfullDemo2023.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult GetAllEmployees()
+        {
+
+            List<EmployeeModel> listEmp = new List<EmployeeModel>();
+            listEmp = FillEmpoloyeeModel();
+            return View(listEmp);
+        }
+
+
+        // GET: EmployeeByID
+        public ActionResult GetEmployeeByID(int ID)
+        {
+
+            EmployeeModel Emp = new EmployeeModel();
+            Emp = FillEmpoloyeeModel().Where(e=>e.ID==ID).FirstOrDefault();
+            return View(Emp);
+        }
+
+
+
+        public List<EmployeeModel> FillEmpoloyeeModel()
+        {
+            List<EmployeeModel> listEmp = new List<EmployeeModel>();
+            listEmp.AddRange(new List<EmployeeModel>
+            {
+                new EmployeeModel
+                {
+                    ID=1001,Name="Bandi Vamshi Krishna",PhoneNo=9999999999,Email="vamshi@gmail.com",DOB="30/04/1991",
+                    Address="Sai Bhagavan Colony, Ramachandrapuram",City="Hyderabad",
+                    State="Telangana",PinCode=502032,Country="India"
+                },
+                new EmployeeModel
+                {
+                    ID=1002,Name="Vamshi Krishna",PhoneNo=8888888888,Email="bandi@gmail.com",DOB="30/04/1992",
+                    Address="Sai Bhagavan Colony, Ramachandrapuram",City="Hyderabad",
+                    State="Telangana",PinCode=502032,Country="India"
+                },
+                new EmployeeModel
+                {
+                    ID=1003,Name="Krishna",PhoneNo=7777777777,Email="krishna@gmail.com",DOB="30/04/1993",
+                    Address="Sai Bhagavan Colony, Ramachandrapuram",City="Hyderabad",
+                    State="Telangana",PinCode=502032,Country="India"
+                },
+                new EmployeeModel
+                {
+                    ID=1004,Name="Vamshi",PhoneNo=6666666666,Email="bvk@gmail.com",DOB="30/04/1994",
+                    Address="Sai Bhagavan Colony, Ramachandrapuram",City="Hyderabad",
+                    State="Telangana",PinCode=502032,Country="India"
+                }
+            });
+            return listEmp;
+        }
+    }
+}
---
+++ MVCfullDemo2023/MVCfullDemo2023/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCfullDemo2023.Models
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DOB { get; set; }
+        public string Email { get; set; }
+        public Int64 PhoneNo { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PinCode { get; set; }
+        public string Country { get; set; }
+    }
+}
